ac222703df40e6bdc2118e18fd119aa3e3b2b411
AwareJooJ <alvaroalexandre@proton.me>
2026-07-13 13:09:03 -0300

    Add 16x16 squares and add button

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const body = document.querySelector("body");
 const container = document.getElementById("container");
-const square = document.createElement("div");
+const btn = document.createElement("button");
+
+btn.textContent = "Grids"
+body.appendChild(btn);
+
+for (let i = 1; i <= 256; i++){
+    const square = document.createElement("div");
+    square.classList.toggle("square");
+    square.id = `square${i}`;
+    container.appendChild(square);
+}
