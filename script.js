80cc9cca4b356ebd7a5a52975f9361cc4a7b7aec
AwareJooJ <alvaroalexandre@proton.me>
2026-07-15 11:19:46 -0300

    Add random color event listener

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
 const container = document.getElementById("container");
 const btn = document.querySelector("button");
+let hue = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
 
 function createBoxes() {
   let numberPerRow = prompt("Select number (max = 100):");
@@ -16,10 +17,15 @@ function createBoxes() {
         row.appendChild(collumn);
         collumn.style.width = `${960 / numberPerRow}px`;
         collumn.style.height = `${960 / numberPerRow}px`;
+        collumn.addEventListener("mouseover", () => {
+          let op = 100;
+          let hue = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}, ${op}%)`;
+          collumn.style.backgroundColor = hue;
+        });
       }
     }
   } else {
-    alert("Select a valid number")
+    alert("Select a valid number");
   }
 }
 
